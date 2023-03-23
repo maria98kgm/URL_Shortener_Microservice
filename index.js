bf8c20adc657562ace2b23f5886fce8c237c3ef3
Marie Kagermanova <maria.kgm04@gmail.com>
2023-03-23 17:40:32 +0300

    add route that generates short path; add route that redirects to original url

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
 
 // Basic Configuration
@@ -8,17 +8,40 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+// route that generates short url
+const urls = [];
+
+app.post("/api/shorturl", function (req, res) {
+  const pattern = new RegExp(
+    "^(https?:\\/\\/)?" + // protocol
+      "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
+      "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
+      "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
+      "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
+      "(\\#[-a-z\\d_]*)?$", // fragment locator
+    "i"
+  );
+
+  if (!pattern.test(req.body.url)) res.json({ error: "invalid url" });
+
+  urls.push({ original: req.body.url, id: urls.length + 1 });
+  res.json({ original_url: req.body.url, short_url: urls.length });
 });
 
-app.listen(port, function() {
+// route that redirects to original url
+app.get("/api/shorturl/:shortPath", function (req, res) {
+  const originalPath = urls.find(
+    (item) => item.id === Number(req.params.shortPath)
+  );
+  res.redirect(originalPath.original);
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
